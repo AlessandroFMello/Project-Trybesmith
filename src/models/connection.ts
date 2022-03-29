954827b09ebc6f999175abe4fc00dfcbd6fca0f3
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-03-29 16:49:19 -0300

    fix: fix config AGAIN

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -2,10 +2,12 @@ import 'dotenv/config';
 import mysql from 'mysql2/promise';
 import { PrismaClient } from '@prisma/client';
 
+const { MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD } = process.env;
+
 const connection = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
+  host: MYSQL_HOST,
+  user: MYSQL_USER,
+  password: MYSQL_PASSWORD,
 });
 
 export default connection;
@@ -13,8 +15,7 @@ export default connection;
 export const prisma = new PrismaClient({
   datasources: {
     db: {
-      url: `mysql://${process.env.MYSQL_USER}:
-      ${process.env.MYSQL_PASSWORD}@${process.env.MYSQL_HOST}:3306/Trybesmith`,
+      url: `mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}:3306/Trybesmith`,
     },
   },
 });
