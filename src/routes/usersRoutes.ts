fea3a1db04b798e3e3ecf3271b0651f166228210
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-03-29 18:32:01 -0300

    feat: creates post route

--- src/routes/usersRoutes.ts
+++ src/routes/usersRoutes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
+import UsersController from '../controllers/usersController';
+import usersValidationMiddleware from '../middlewares/usersValudationMiddleware';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+const usersController = new UsersController();
+
+router.get('/', usersController.getAll);
+
+router.post('/', usersValidationMiddleware, usersController.createUser);
 
 export default router;
