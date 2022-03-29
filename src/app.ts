cfc208ad9706cf2ab45a656e95cbf1eb0a3bc928
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-03-29 15:34:23 -0300

    feat: configures routes endpoints

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
 import express from 'express';
+import { ordersRoutes, productsRoutes, usersRoutes } from './routes';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/orders', ordersRoutes);
+
+app.use('/products', productsRoutes);
+
+app.use('/users', usersRoutes);
 
 export default app;
