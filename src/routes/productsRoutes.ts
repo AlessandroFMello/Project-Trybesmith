8058b7837eabdc3c403c1a11dcb1d05f266c26f6
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-03-29 17:07:57 -0300

    feat: creates post route

--- src/routes/productsRoutes.ts
+++ src/routes/productsRoutes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import ProductsController from '../controllers/productsController';
+import productsValidationMiddleware from '../middlewares/joiValidationMiddleware';
 
 const router = Router();
 
 const productController = new ProductsController();
 
 router.get('/', productController.getAll);
+
+router.post('/', productsValidationMiddleware, productController.createProduct);
 
 export default router;
