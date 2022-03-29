5c4fe81dfaddeacadad91fc36a2777aaf8c2d918
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-03-29 18:29:00 -0300

    fix: changes wrong names

--- src/controllers/productsController.ts
+++ src/controllers/productsController.ts
@@ -2,16 +2,16 @@ import { Request, Response } from 'express';
 import ProductsService from '../services/productsService';
 
 class ProductsController {
-  constructor(private bookService = new ProductsService()) { }
+  constructor(private productsService = new ProductsService()) { }
 
   public getAll = async (_req: Request, res: Response) => {
-    const { code, allProducts } = await this.bookService.getAll();
+    const { code, allProducts } = await this.productsService.getAll();
     return res.status(code).json(allProducts);
   };
 
   public createProduct = async (req: Request, res: Response) => {
     const { name, amount } = req.body;
-    const { code, newProduct } = await this.bookService.createProduct({
+    const { code, newProduct } = await this.productsService.createProduct({
       name,
       amount,
     });
