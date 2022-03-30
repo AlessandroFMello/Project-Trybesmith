f8fd820a4f6ab90e837690f600bc096f90094331
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-03-30 16:30:58 -0300

    feat: login router indexing

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,3 +1,4 @@
 export { default as ordersRoutes } from './ordersRoutes';
 export { default as productsRoutes } from './productsRoutes';
 export { default as usersRoutes } from './usersRoutes';
+export { default as loginRoutes } from './loginRoutes';
