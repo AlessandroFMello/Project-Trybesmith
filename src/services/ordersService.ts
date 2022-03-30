02e4663de2c4248035303330529de391d739e04b
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-03-30 13:40:25 -0300

    fix: corrects order services after rerunning prisma generate

--- src/services/ordersService.ts
+++ src/services/ordersService.ts
@@ -2,9 +2,9 @@ import { prisma } from '../models/connection';
 
 class OrdersService {
   public getAll = async () => {
-    const allOrders = await prisma.orders.findMany({ 
+    const allOrders = await prisma.order.findMany({ 
       include: {
-        Products: {
+        products: {
           select: { id: true },
         },
       },
@@ -14,7 +14,7 @@ class OrdersService {
     const validatedOrders = allOrders.map((order) => ({
       id: order.id,
       userId: order.userId,
-      products: order.Products.map((product) => product.id), // adiciona a propriedade products que contem apenas os ids dos produtos
+      products: order.products.map((product) => product.id), // adiciona a propriedade products que contem apenas os ids dos produtos
     }));
 
     return { code: 200, validatedOrders };
